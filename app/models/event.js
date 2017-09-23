75bfcdc4d20a5e8546b028105e51f2ee77f3edc8
JacqueWhite <jacquecwhite@gmail.com>
2017-09-23 16:28:40 -0500

    adding event model

--- app/models/event.js
+++ app/models/event.js
@@ -3,7 +3,29 @@
 
 module.exports = function(sequelize, DataTypes) {
 
-var Event = sequelize.define("User", {
+	var Event = sequelize.define("Event", {
+	  userId: {
+	    type: Sequelize.STRING
+	  },
+	  eventName: {
+	    type: Sequelize.STRING
+	  },
+	  eventAddress: {
+	    type: Sequelize.STRING
+	  },
+	  eventCity: {
+	    type: Sequelize.STRING
+	  },
+	  eventState: {
+	    type: Sequelize.STRING
+	  },
+	  eventZip: {
+	    type: Sequelize.STRING
+	  },
+	  eventCost: {
+	    type: Sequelize.DECIMAL
+	  }
+	}
 });
 
 return Event;
